9cd76029b9fbb0d48a139e3654157f348b38c97d
manoelvsalgado <87504063+manoelvsalgado@users.noreply.github.com>
2023-04-19 18:13:49 -0300

    Add files via upload

---
+++ api/src/controllers/UsersController.js
@@ -0,0 +1,26 @@
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const database = await sqliteConnection();
+    const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?), [email]");
+
+    if(checkUserExist){
+      throw new AppError("Este e-mail já está em uso.");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALEUS (?, ?, ?)",
+    [ name, email, hashedPassword ]
+    );
+
+    return response.status(201).json();
+  }
+}
+
+module.exports = UsersController;
