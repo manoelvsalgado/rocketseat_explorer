b20243a075ac8fac8a10ff7c9b1cd18067ce6ae2
manoelvsalgado <87504063+manoelvsalgado@users.noreply.github.com>
2023-04-25 16:18:29 -0300

    Add files via upload

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,6 +1,8 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
+const { request } = require("https");
+const { response } = require("express");
 
 class UsersController {
   async create(request, response) {
@@ -21,6 +23,52 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update (request, response) {
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if( password && !old_password){
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha.");
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+      
+      if(!checkOldPassword) {
+        throw new AppError("A senha não confere.")
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+      );
+
+      return response.json();
+ }
 }
 
 module.exports = UsersController;
